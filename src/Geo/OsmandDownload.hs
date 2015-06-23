39d1b26777c05ef40fa373fd20c66ec363b1c594
Tony Morris <tmorris@tmorris.net>
2015-06-23 20:16:09 +1000

    osmand-download

--- src/Geo/OsmandDownload.hs
+++ src/Geo/OsmandDownload.hs
@@ -1,1 +1,135 @@
+{-# LANGUAGE CPP #-}
+
 module Geo.OsmandDownload where
+
+#if !(MIN_VERSION_base(4,8,0))
+import Control.Applicative(Applicative((<*>), pure))
+#endif
+import Data.Time(UTCTime(utctDay, utctDayTime), TimeOfDay(TimeOfDay), toGregorian, timeToTimeOfDay)
+import Sys.Exit(CreateProcess, procIn)
+import System.FilePath((</>))
+
+time ::
+  UTCTime
+  -> String
+time t =
+  let show2 = let s2 [x] = ['0', x]
+                  s2 x = x
+              in s2 . show
+      (y, m, d) = toGregorian (utctDay t)
+      TimeOfDay h n s = timeToTimeOfDay (utctDayTime t)
+  in concat [show y, show2 m, show2 d, "-", show2 h, show2 n, show2 (floor s)]
+
+downloadDirectory ::
+  FilePath
+downloadDirectory =
+  "download"
+
+buildDirectory ::
+  FilePath
+buildDirectory =
+  "build"
+
+distDirectory ::
+  FilePath
+distDirectory =
+  "dist"
+
+getObf ::
+  FilePath
+  -> FilePath
+  -> CreateProcess
+getObf obf wd =
+  let z = obf ++ ".obf.zip"
+  in procIn (wd </> downloadDirectory) "wget"
+        [
+          "-c"
+        , "http://download.osmand.net/download.php?standard=yes&file=" ++ z
+        , "-O"
+        , z
+        ]
+
+unzipObf ::
+  FilePath
+  -> FilePath
+  -> CreateProcess
+unzipObf obf wd =
+  let z = obf ++ ".obf.zip"
+  in procIn wd "unzip"
+        [
+          "-d"
+        , wd </> buildDirectory
+        , downloadDirectory </> z
+        ]
+
+renameObf ::
+  FilePath
+  -> FilePath
+  -> FilePath
+  -> CreateProcess
+renameObf obf1 obf2 wd =
+  procIn (wd </> buildDirectory) "mv"
+    [
+      "-v"
+    , obf1 ++ ".obf"
+    , obf2 ++ ".obf"
+    ]
+
+distObf ::
+  FilePath
+  -> FilePath
+  -> CreateProcess
+distObf obf wd =
+  procIn (wd </> distDirectory) "ln"
+    [
+      "-s"
+    , ".." </> buildDirectory </> obf ++ ".obf"
+    ]
+
+australiaOceania ::
+  FilePath
+  -> [CreateProcess]
+australiaOceania =
+  sequence
+    [
+      getObf "Australia-oceania_2"
+    , unzipObf "Australia-oceania_2"
+    , renameObf "Australia-oceania_2" "Australia-oceania"
+    , distObf "Australia-oceania"
+    ]
+
+indonesiaAsia ::
+  FilePath
+  -> [CreateProcess]
+indonesiaAsia =
+  sequence
+    [
+      getObf "Indonesia_asia_2"
+    , unzipObf "Indonesia_asia_2"
+    , renameObf "Indonesia_asia_2" "Indonesia_asia"
+    , distObf "Indonesia_asia"
+    ]
+
+commands ::
+  FilePath
+  -> [CreateProcess]
+commands =
+  fmap concat . sequence $
+    [
+      australiaOceania
+    , indonesiaAsia
+    ]
+
+linkLatest ::
+  FilePath
+  -> String
+  -> CreateProcess
+linkLatest d t =
+  procIn d "ln"
+    [
+      "-f"
+    , "-s"
+    , "-n"
+    , t
+    , "latest"
+    ]
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,32 @@
-module Main where
-     
+module Main(
+  run
+, main
+) where
+
+import Control.Monad.Trans.Class(MonadTrans(lift))
+import Data.NotZero(notZeroElse1)
+import Data.NotZeroOr(NotZeroOr(IsNotZero))
+import Data.Time(getCurrentTime)
+import Geo.OsmandDownload(time, commands, linkLatest)
+import System.Environment(getArgs)
+import Sys.Exit(ExitCodeM, createMakeWaitProcesses, createMakeWaitProcessM, exitWith, exit)
+import System.FilePath((</>))
+import System.IO(hPutStrLn, stderr)
+
+run ::
+  FilePath
+  -> ExitCodeM IO
+run d =
+  do t <- lift getCurrentTime
+     let u = time t
+     let e = d </> u 
+     createMakeWaitProcesses (commands e)
+     createMakeWaitProcessM (linkLatest d e)
+       
 main ::
   IO ()
 main =
-  return ()
+  do a <- getArgs
+     case a of
+       [] -> hPutStrLn stderr "Usage: osmand-download <output-dir>" >> exitWith (IsNotZero (notZeroElse1 129))
+       (o:_) -> exit (run o)
